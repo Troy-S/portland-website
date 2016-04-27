508caa8d1d939ffe8b3a277f25bbf87c5698fdbb
Troy Sheed <tsheed@hotmail.co.uk>
2016-04-27 15:02:42 +0100

    Saving progress... Amjal, Amuse, CoffeeWeek, HomePage...

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 angular
-	.module('portlandApp', ['ui.router', 'portlandApp.controllers', 'ngMaterial'])
+	.module('portlandApp', ['ui.router', 'portlandApp.controllers', 'ngMaterial', 'ngAnimate'])
 	.config(MainRouter)
 
 MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
@@ -14,6 +14,16 @@ function MainRouter($stateProvider, $urlRouterProvider) {
 			url: "/work",
 			templateUrl: "/templates/work.html",
 			controller: "WorkController"
+		})
+		.state('amuse', {
+			url: "/amuse",
+			templateUrl: "/templates/amuse.html",
+			controller: "AmuseController"
+		})
+		.state('amjal', {
+			url: "/amjal",
+			templateUrl: "/templates/amjal.html",
+			controller: "AmjalController"
 		})
 
 		$urlRouterProvider.otherwise('/home');
--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,22 @@
 angular.module('portlandApp.controllers', [])
 
 .controller('HomeController', function($scope) {
-	
+	$scope.pageClass = 'page-home';
 })
 
 .controller('WorkController', function($scope) {
+	$scope.pageClass = 'page-work';
+})
 
+.controller('AmuseController', function($scope, $mdSidenav) {
+	$scope.openRightMenu = function() {
+		$mdSidenav('right').toggle();
+		console.log($scope.openRightMenu);
+	}
+
+	$scope.pageClass = 'page-amuse';
+})
+
+.controller('AmjalController', function($scope) {
+	
 })
